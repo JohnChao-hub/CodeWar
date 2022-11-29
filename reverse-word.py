8dd9a5e814686e35bdcb057312496f71c4d98642
JohnChao-hub <cc2145a@student.american.edu>
2022-11-29 02:24:35 -0500

    second

---
+++ reverse-word.py
@@ -0,0 +1,14 @@
+## Reverse word
+a = ("abcde")
+def reverse_words(str):
+  strList = []
+  for word in str.split(' '):
+      strList.append(word[::-1])
+  return ' '.join(strList)
+
+reverse_words(a)
+
+# second sol
+def solution(str):
+  return str[::-1]
+solution('world')
